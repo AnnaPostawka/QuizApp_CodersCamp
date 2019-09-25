72a66c8264e03885fc2fc4426f03232f2556112f
deBroglie <tanski.gustaw@gmail.com>
2019-09-25 18:59:42 +0200

    GitHooks fixed

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,7 +5,8 @@ import LandingView from "./views/LandingView";
 export default function App() {
 	return (
 		<HashRouter>
-			<Switch><Route to="/" component={LandingView} />
+			<Switch>
+				<Route to="/" component={LandingView} />
 			</Switch>
 		</HashRouter>
 	);
