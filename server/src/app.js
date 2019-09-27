d5ec72585a0c549532e0ac98d2bd3220da560a5e
anna-postawka <anna.postawka@gmail.com>
2019-09-27 18:41:11 +0200

    server connection using express

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,10 @@
-function dog(pieski) { console.log("woof!");
-	console.log("im dog" == "psy");
-}
-process;
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`);
+
+	//connect to database
+});
