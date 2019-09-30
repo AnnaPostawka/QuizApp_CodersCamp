228c4a138e4d636e767e909214274f512a45958d
Izabela Popiel <izabelapopiel1@gmail.com>
2019-09-30 16:05:44 +0200

    userModel basic version

---
+++ server/src/models/userModel.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User; 
+exports.validate = validateUser;
