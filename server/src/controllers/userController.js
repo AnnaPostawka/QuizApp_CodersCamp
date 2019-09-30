b586ca803a1f4357d8d888f3a5bc3eb5e2af6511
Izabela Popiel <izabelapopiel1@gmail.com>
2019-09-30 16:34:28 +0200

    registerUser added

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,20 @@
+const { User, validate } = require("../models/userModel");
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+
+class UserController {
+	async registerUser(req, res) {
+		const { error } = validate(req.body);
+		if (error) return res.status(400).send(error.details[0].message);
+
+		let user = await User.findOne({ email: req.body.email });
+		if (user) return res.status(400).send("User already registered.");
+
+		user = new User(_.pick(req.body, ["email", "password"]));
+		const salt = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hash(user.password, salt);
+		await user.save();
+	}
+}
+
+export default UserController;
