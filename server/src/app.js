d93bb5184f90934d5992730725171f774af32cec
anna-postawka <anna.postawka@gmail.com>
2019-10-02 22:51:11 +0200

    dotenv

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 // fixing deprecation warnings
 mongoose.set("useNewUrlParser", true);
